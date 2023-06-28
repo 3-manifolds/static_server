cf8c31d0c40b6e77c94dfe052c60d7395104ea74
Nathan Dunfield <nathan@dunfield.info>
2023-06-27 19:44:56 -0500

    Down with setup.py, up with pyproject.toml!

--- src/cocoserver/__init__.py
+++ src/cocoserver/__init__.py
@@ -11,7 +11,7 @@ import webbrowser
 import urllib.parse
 import importlib.metadata
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 class GzipHTTPRequestHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
